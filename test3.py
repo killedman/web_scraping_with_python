ca820ab9d6802820cfe959b027dd1e0160b23884
dreampython <killedmanbylonely@gmail.com>
2021-06-13 15:35:28 +0800

    modify python default user agent

--- test3.py
+++ test3.py
@@ -7,3 +7,15 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 print(builtwith.parse('https://thinknotes.cn'))
 print(builtwith.parse('http://ruanyifeng.com'))
+
+
+"""
+寻找网站所有者
+我们可以使用WHOIS协议查看域名的注册者是谁。
+Python中有一个针对该协议的封装库
+其文档地址为https://pypi.python.org/pypi/python-whois，我们可以通过pip进行安装
+python3 -m pip install python-whois
+注意：测试发现whois无法查询到.cn的域名信息，可以通过站长工具的whois功能查询
+"""
+# import whois
+# print(whois.whois('ruanyifeng.com'))
--- test_can_running.py
+++ test_can_running.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python3
 # why just import urllib is error
 # import urllib
-import urllib.request
+from urllib.request import Request, urlopen  # Python 3
+from urllib.error import HTTPError
+# import urllib.request
 # why not urllib.error ,just use urllib is error
 from urllib.error import URLError
 # resolv question [SSL: CERTIFICATE_VERIFY_FAILED] 
@@ -10,18 +12,27 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
-def download(url):
+def download(url, user_agent='wswp', num_retries=2):
     welcome = f"Downloading: {url}"
     print(welcome)
+    req = Request(url)
+    req.add_header('User-agent', user_agent)  # 修改Python默认的User-agent
     try:
-        html = urllib.request.urlopen(url).read()
-    except urllib.error.URLError as e:
-        print(f"Download error:{e.reason}")
+        html = urlopen(req).read()  # urlopen(req).headers返回headers
+    except HTTPError as e:
+        print(f"Download error:{e.code, e.reason}")
         html = None
+        if num_retries > 0:
+            if hasattr(e, 'code') and 500 <= e.code < 600:
+                print(e.code)
+                # recursively retry 5xx HTTP errors
+                return download(url, user_agent, num_retries-1)
     return html
 
 
 website = 'https://www.kawabangga.com/posts/4249'
+# website = 'http://httpstat.us/500'
 result = download(website)
 if result:
-    print(result.decode('utf8'))
+    # print(result.decode('utf8'))
+    pass
