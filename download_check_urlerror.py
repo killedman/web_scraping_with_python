6e4c29b03a4af28957e6918c4162dc8be20b2ed6
dreampython <killedmanbylonely@gmail.com>
2021-06-14 10:22:37 +0800

    renamed two file that begin test

--- download_check_urlerror.py
+++ download_check_urlerror.py
@@ -1,13 +1,14 @@
 #! /usr/bin/env python3
-import urllib
+from urllib.request import urlopen
+from urllib.error import URLError
 
 
 def download(url):
     welcome = f"Downloading: {url}"
     print(welcome)
     try:
-        html = urllib.request.urlopen(url).read()
-    except urllib.error.URLError as e:
+        html = urlopen(url).read()
+    except URLError as e:
         print(f"Download error:{e.reason}")
         html = None
     return html
